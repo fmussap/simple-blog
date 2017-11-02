66efba77defcf9feaff14f3f921792e7d3d7910a
fmussap <fabiomussap@yahoo.com>
2017-11-02 17:28:21 +0000

    update 02/11

--- src/components/posts-new/index.js
+++ src/components/posts-new/index.js
@@ -5,17 +5,28 @@ import { Link } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
-  renderTitleField (field) {
+  constructor () {
+    super()
+    this.onSubmit = (values) => {
+      console.log('values', values)
+    }
+  }
+  renderField (field) {
     return (
-      <div>
+      <div className='form-group'>
+        <label>{field.label}</label>
         <input
+          className='form-control'
           type='text'
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   }
+
   render () {
+    const { handleSubmit } = this.props
     return (
       <div>
         <div className='text-xs-right'>
@@ -23,17 +34,54 @@ class PostsNew extends Component {
             Back
           </Link>
         </div>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
           <Field
             name='title'
-            component={this.renderTitleField}
+            label='Title'
+            component={this.renderField}
           />
+          <Field
+            name='categories'
+            label='Categories'
+            component={this.renderField}
+          />
+          <Field
+            name='content'
+            label='Post Content'
+            component={this.renderField}
+          />
+          <button type='submit' className='btn btn-primary'>Submit</button>
         </form>
       </div>
     )
   }
 }
 
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = 'Please enter a title'
+  } else if (values.title.length < 3) {
+    errors.title = 'Title must be at least 3 characters'
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Please enter some categories'
+  } else if (values.categories.length < 3) {
+    errors.categories = 'categories must be at least 3 characters'
+  }
+
+  if (!values.content) {
+    errors.content = 'Please write some content'
+  } else if (values.content.length < 3) {
+    errors.content = 'content must be at least 3 characters'
+  }
+
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew)
